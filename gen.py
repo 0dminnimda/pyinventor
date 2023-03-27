f07a8bb4176ab167db71781764826c82f5177c75
0dminnimda <0dminnimda@gmail.com>
2023-03-27 15:13:53 +0300

    Create gen.py

---
+++ gen.py
@@ -0,0 +1,61 @@
+import win32com
+import win32com.client as wincom
+import os
+import shutil
+from pathlib import Path
+
+
+def GetDispatchPath(disp: wincom.DispatchBaseClass) -> Path:
+    # Get the clsid, lcid, major, and minor for the Dispatch object
+    ti = disp._oleobj_.GetTypeInfo()
+    disp_clsid = ti.GetTypeAttr()[0]
+    tlb, index = ti.GetContainingTypeLib()
+    clsid, lcid, major, minor, *_ = tlb.GetLibAttr()
+
+    # Get the path of the generated file
+    filename = wincom.gencache.GetGeneratedFileName(clsid, lcid, major, minor)
+    return Path(win32com.__gen_path__) / filename
+
+
+# def GenerateToDirectory(name: str, directory: Path) -> wincom.DispatchBaseClass:
+#     disp = wincom.gencache.EnsureDispatch(name)
+#     source = GetDispatchPath(disp)
+#     target = Path(directory) / source.name
+#     shutil.move(source, target)
+#     return disp
+
+
+META = "metafile.txt"
+
+
+def GetStoredMtime(directory: Path) -> float:
+    metafile = destination / META
+    if metafile.exists():
+        return float(metafile.read_text())
+    return -1.
+
+
+def SetStoredMtime(directory: Path, mtime: float) -> None:
+    metafile = destination / META
+    metafile.write_text(str(mtime))
+
+
+def DispatchToDirectory(name: str, directory: Path, force: bool = False) -> wincom.DispatchBaseClass:
+    disp = wincom.gencache.EnsureDispatch(name)
+    source = GetDispatchPath(disp)
+
+    mtime = source.stat().st_mtime
+    if not force and mtime == GetStoredMtime(destination):
+        return disp
+
+    destination = Path(directory)
+    if destination.exists():
+        shutil.rmtree(destination)
+
+    shutil.copytree(source, destination)
+    SetStoredMtime(destination, mtime)
+
+
+if __name__ == "__main__":
+    directory = Path(__file__).parent / "com"
+    DispatchToDirectory("Inventor.Application", directory)
