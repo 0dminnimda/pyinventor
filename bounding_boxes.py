f777b7eac3aa9ae9c8237bdfbdcacb363b3b1657
0dminnimda <0dminnimda@gmail.com>
2024-10-10 19:07:24 +0300

    Update bounding_bozes.py

--- bounding_boxes.py
+++ bounding_boxes.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from dataclasses import dataclass, field
+from collections import deque
 
 from pyinventor import (
     Inventor,
@@ -28,76 +29,87 @@ from pyinventor import (
 BodySize = tuple[float, float, float]
 
 
+ROUND_TO_NDIGITS = 0
+
+
 def get_body_size(body: com.SurfaceBody) -> BodySize:
     bounding_box: com.OrientedBox = body.OrientedMinimumRangeBox
 
     return tuple(sorted(
         [
-            round(bounding_box.DirectionOne.Length, 3),
-            round(bounding_box.DirectionTwo.Length, 3),
-            round(bounding_box.DirectionThree.Length, 3),
+            round(bounding_box.DirectionOne.Length, ROUND_TO_NDIGITS),
+            round(bounding_box.DirectionTwo.Length, ROUND_TO_NDIGITS),
+            round(bounding_box.DirectionThree.Length, ROUND_TO_NDIGITS),
         ],
         reverse=True,
     ))
 
 
-
+@dataclass
 class SurfaceBodyVisitor:
+    predecessors: deque = field(default_factory=deque)
+
     def traverse_assembly(self, assembly_document):
         assembly_document = cast_to(assembly_document, com.AssemblyDocument)
+
+        self.predecessors.append(assembly_document)
         self.traverse_occurrences(assembly_document.ComponentDefinition.Occurrences)
+        self.predecessors.pop()
 
     def traverse_occurrences(self, occurrences):
         occurance: com.ComponentOccurrence
         for occurance in occurrences:
+            self.predecessors.append(occurance)
             if occurance.DefinitionDocumentType == const.kPartDocumentObject:
                 for body in occurance.SurfaceBodies:
                     self.visit_surface_body(body)
 
             self.traverse_occurrences(occurance.SubOccurrences)
+            self.predecessors.pop()
 
     def visit_surface_body(self, body: com.SurfaceBody):
         pass
 
 
-_get_name_cache = {}
-
-
-def get_name(obj) -> str:
-    key = hash((69, id(obj)))
-    if key in _get_name_cache:
-        return _get_name_cache[key]
-
+def get_name(obj, with_parents: bool = True, *, first: bool = True) -> str:
     result = ""
     if hasattr(obj, "Name"):
         result = obj.Name
     elif hasattr(obj, "DisplayName"):
         result = obj.DisplayName
-    # elif not result:
-    #     result = "<Без Имени>"
+    elif first:
+        result = "<Без Имени>"
 
-    if hasattr(obj, "Parent"):
-        parent_result = get_name(obj.Parent)
+    if with_parents and hasattr(obj, "Parent"):
+        parent_result = get_name(obj.Parent, first=False)
         if parent_result and result:
             parent_result += " : "
         result = parent_result + result
 
-    _get_name_cache[key] = result
     return result
 
 
 @dataclass
 class SurfaceBodySizeTaker(SurfaceBodyVisitor):
-    table: list[tuple[str, BodySize]] = field(default_factory=list)
+    table: list[tuple] = field(default_factory=list)
 
     def visit_surface_body(self, body: com.SurfaceBody):
-        self.table.append((get_name(body), get_body_size(body)))
+        predecessors = [
+            get_name(it, False, first=False)
+            for it in self.predecessors
+        ] + [get_name(body, False)]
+        self.table.append((
+            get_name(body),
+            " : ".join(it for it in predecessors if it),
+            get_name(body, False),
+            *get_body_size(body)
+        ))
 
 
-def dump_size_table_as_csv(table: list[tuple[str, BodySize]]) -> str:
+def dump_size_table_as_csv(table: list[tuple]) -> str:
     result = ""
-    for name, size in table:
-        result += f"{name};" + ";".join(map(str, size)) + "\n"
+    for it in table:
+        result += ";".join(map(str, it)) + "\n"
     return result
 
 
